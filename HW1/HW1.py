8ed040ebb59f445203a069277a4aeea73b1355b2
unknown <shadmanskhan@berkeley.edu>
2025-02-03 13:33:31 -0800

    Adding HW1

---
+++ HW1/HW1.py
@@ -0,0 +1,81 @@
+"""
+1
+pwd=
+a
+	The command is Print Working Directory.
+b
+	It can be used to see where one is working.
+c
+	Extensions???????	
+
+cd: 
+a
+	Change Directory. Changes the directory to another
+b
+	Used to change the current working folder into another
+c
+	MUST be used with any other word to change to a folder with that name should it exist.
+	cd filename
+
+ls:
+a
+	Lists all folders and files inside the current WD
+b
+	To see all files and can be used before cd to get a look at possible file paths.
+c
+	EXTENSIONS??! EXTENSIONS!?!?!?
+
+mkdir
+a
+	Makes a new folder
+b
+	Is used to make a folder. That is it. It makes a folder and then that file now exists.
+c
+	Any word will make a folder of that name. mkdir filename
+rm
+a
+	Removes a file
+b
+	Removes a file you might no longer want/need. Potential to do actually damage to the system.
+c
+	-r removes the entire folder(directory)
+cp
+a
+	copy
+b
+	Copies a file 
+c
+	-r copies an entire folder.
+
+2
+	Git is the software allowing things we want to happen to happen as necessary
+	Bash allows the user to interract with the Git. Github allows the sharing of the source
+	code/files with others. It is in essence, a cloud system.
+	
+	Terminal is the GUI part where the commands are written and the Command Line is the place 
+	with the comamnd that is being written.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
