376f540332a093b60e9c33fe4abda02da2cd43e5
unknown <shadmanskhan@berkeley.edu>
2025-02-10 00:24:43 -0800

    Holy hecc

--- HW2/homework2/hw2.py
+++ HW2/homework2/hw2.py
@@ -1,10 +1,34 @@
+'''
 # 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+    cd ~/Desktop or run cd .. twice
+
 # 2) What does ~/ mean?
+    The home directory, the main directory which stores many other necessary directories
+
 # 3) What's the difference between and abolute and a relative path in your terminal?
+    The absolute path is going directly from home to anywhere else. A relative path is the path taken from other folders
+    Absolute ~/Whatever/Wherever/However  while a relative path would be ../Whenever/Whyever
+
 # 4) What command would bring you back to your home directory?
+    cd ~
+
 # 5) If you called rm ./ in your current working directory, what would it do? 
+    nothing??? It will likely throw up an error. If it does not, it will empty the folder leaving the husk
+
 # 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+    Git add puts everything in your .git directory into the changes to be committed. Commit caches it on your local and push uploads it
+
 # 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+    On branch master, Working Tree Clean No commits yet and list untracked file. It tells you that the local copy is not committed
+
 # 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+    The need for permission, adding and verifying email fixed it easily. 
+
 # 9) How does cloning a repository differ from pulling from a repository?
+    Cloning the repositoy will add the frameworks and everything in the repository. Pulling from it only adds tracked changes, and will not replace files if accidentally deleted(Tested)
+
 # 10) Tell me a fun fact!
+    There is a fruit named the Devil's Apple(Linnaeanum) in the Solanum genus, also containing potatoes(Tuberosum), Tomatoes(Lycopersicum) and such. The fruit is apparently poisonous but that does not stop certain villages in Kenya(according to Wikipedia) from using it as a football during summer.
+
+
+'''
